0525f3a1b749a446b74ef2ddb15b72e895c25ec8
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-15 16:14:25 +0900

    Expose coroutine types from lyrics core API

--- core/lyrics/build.gradle.kts
+++ core/lyrics/build.gradle.kts
@@ -9,7 +9,7 @@ kotlin {
 dependencies {
     api(project(":core:model"))
     api(project(":provider:api"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
     testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.11.0")
 }
