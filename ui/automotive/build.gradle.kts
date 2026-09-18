eb8be3f480f646206fca051b789b9f142db47f29
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-19 01:40:25 +0900

    feat: prepare automotive media host dependencies

--- ui/automotive/build.gradle.kts
+++ ui/automotive/build.gradle.kts
@@ -31,4 +31,9 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.media:media:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
+
+    testImplementation(kotlin("test-junit"))
 }
