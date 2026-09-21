83c1720e170a901f71d5f1d21e101de854af6504
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-21 11:59:56 +0900

    style: add softer shell chrome color

--- ui/designsystem/src/main/java/io/github/whoxamxl/aalyrics/ui/designsystem/theme/Color.kt
+++ ui/designsystem/src/main/java/io/github/whoxamxl/aalyrics/ui/designsystem/theme/Color.kt
@@ -31,6 +31,7 @@ object AALyricsPalette {
 object AALyricsColors {
     val BackgroundBase = AALyricsPalette.Navy950
     val BackgroundSurface = Color(0xFF071524)
+    val BackgroundChrome = BackgroundSurface.copy(alpha = 0.72f)
     val BackgroundSurfaceStrong = Color(0xFF0B2038)
     val TextPrimary = AALyricsPalette.Neutral50
     val TextSecondary = AALyricsPalette.Neutral300
