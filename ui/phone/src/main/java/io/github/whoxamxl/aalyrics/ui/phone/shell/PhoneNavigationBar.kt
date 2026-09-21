60a72c311a0fad912cba863b2b151e0f8e94bfe8
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-21 12:00:16 +0900

    style: match Phone navigation background

--- ui/phone/src/main/java/io/github/whoxamxl/aalyrics/ui/phone/shell/PhoneNavigationBar.kt
+++ ui/phone/src/main/java/io/github/whoxamxl/aalyrics/ui/phone/shell/PhoneNavigationBar.kt
@@ -38,7 +38,7 @@ fun PhoneNavigationBar(
 ) {
     Surface(
         modifier = modifier.fillMaxWidth(),
-        color = AALyricsColors.BackgroundSurface,
+        color = AALyricsColors.BackgroundChrome,
     ) {
         Row(
             modifier = Modifier
