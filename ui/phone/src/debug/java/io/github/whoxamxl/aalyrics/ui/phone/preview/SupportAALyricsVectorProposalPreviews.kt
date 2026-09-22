85479ae2000d8a213579dfd4ed4e94b2f828477e
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-22 14:54:55 +0900

    fix(preview): use geometry types

--- ui/phone/src/debug/java/io/github/whoxamxl/aalyrics/ui/phone/preview/SupportAALyricsVectorProposalPreviews.kt
+++ ui/phone/src/debug/java/io/github/whoxamxl/aalyrics/ui/phone/preview/SupportAALyricsVectorProposalPreviews.kt
@@ -22,9 +22,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.CornerRadius
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.Offset
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
