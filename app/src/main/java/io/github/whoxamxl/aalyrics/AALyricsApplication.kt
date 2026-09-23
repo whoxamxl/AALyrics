8272dddedb858f2796dd832a01eaf7840b00f2a5
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-23 18:18:56 +0900

    fix: import update state collector

--- app/src/main/java/io/github/whoxamxl/aalyrics/AALyricsApplication.kt
+++ app/src/main/java/io/github/whoxamxl/aalyrics/AALyricsApplication.kt
@@ -51,6 +51,7 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
