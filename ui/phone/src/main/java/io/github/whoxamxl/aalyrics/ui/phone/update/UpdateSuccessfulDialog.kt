a0fa975a58f0410dd5d12fc286cd942ee2f5edfc
Yuta Miura <86651945+whoxamxl@users.noreply.github.com>
2026-09-24 09:56:11 +0900

    feat: use version chip in update success dialog

--- ui/phone/src/main/java/io/github/whoxamxl/aalyrics/ui/phone/update/UpdateSuccessfulDialog.kt
+++ ui/phone/src/main/java/io/github/whoxamxl/aalyrics/ui/phone/update/UpdateSuccessfulDialog.kt
@@ -28,6 +28,7 @@ import io.github.whoxamxl.aalyrics.ui.designsystem.theme.AALyricsStroke
 import io.github.whoxamxl.aalyrics.ui.designsystem.theme.AALyricsTypography
 import io.github.whoxamxl.aalyrics.ui.phone.R
 import io.github.whoxamxl.aalyrics.ui.phone.component.PhoneDialogHeader
+import io.github.whoxamxl.aalyrics.ui.phone.component.VersionChip
 
 @Immutable
 data class UpdateSuccessfulDialogUiState(
@@ -103,10 +104,13 @@ fun UpdateSuccessfulDialogContent(
 
             Spacer(Modifier.height(AALyricsSpacing.Space16))
 
+            VersionChip(versionName = state.versionName)
+
+            Spacer(Modifier.height(AALyricsSpacing.Space16))
+
             Text(
                 text = stringResource(
                     R.string.update_successful_body,
-                    state.versionName,
                 ),
                 style = AALyricsTypography.TrackArtist,
                 color = AALyricsColors.TextSecondary,
